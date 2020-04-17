fd4c92bd0037d3696885c2a32ce284b8c420345f
Yichi “Vero” Zhang <yichi.zhang@ufl.edu>
2020-04-17 16:58:39 -0400

    Finished non-stick rotate and lone circle long leap.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.qhull import ConvexHull
+import matplotlib.pyplot as plt
 import portion
 
 
@@ -8,8 +9,14 @@ def distance(a, b):
 # distance
 
 
+# return b's relative angle to a, that is, angle between x axis and segment ab
+def angle(a, b):
+    return np.arctan2(b[1] - a[1], b[0] - a[0])
+# angle
+
+
 # return 0 when tangent, 1 when close, 2 when far, and -1 when overlapping
-def relate_dist(a, b):
+def calc_tangency(a, b):
     dist = distance(a, b)
     if np.abs(dist - 1) <= 1e-6:
         return 0
@@ -22,17 +29,11 @@ def relate_dist(a, b):
 # relate_dist
 
 
-# return b's relative angle to a, that is, angle between x axis and segment ab
-def angle(a, b):
-    return np.arctan2(b[1] - a[1], b[0] - a[0])
-# angle
-
-
 # calculate adj matrix for all circles
 def calc_adj_matrix():
     for i in range(num):
         for j in range(num):
-            adj_matrix[i][j] = relate_dist(circles[i], circles[j])
+            adj_matrix[i][j] = calc_tangency(circles[i], circles[j])
     return
 # calc_adj_matrix
 
@@ -40,8 +41,8 @@ def calc_adj_matrix():
 # fix adj matrix for one circle
 def fix_adj_matrix(a):
     for i in range(num):
-        adj_matrix[a][i] = relate_dist(circles[a], circles[i])
-        adj_matrix[i][a] = relate_dist(circles[i], circles[a])
+        adj_matrix[a][i] = calc_tangency(circles[a], circles[i])
+        adj_matrix[i][a] = calc_tangency(circles[i], circles[a])
     return
 # fix_adj_matrix
 
@@ -71,14 +72,15 @@ def calc_near(a):
 # and each pair of closest neighbours has an angle smaller than \pi (180 degree)
 def is_pinned(a):
     neighbour_arr = calc_neighbour(a)
+
+    # need at least 3 neighbours to pin a circle
+    if len(neighbour_arr) < 3:
+        return False
+
     angle_arr = []
     for i in range(len(neighbour_arr)):
         neighbour = neighbour_arr[i]
         angle_arr.append(angle(circles[a], circles[neighbour]))
-
-    # need at least 3 neighbours to pin a circle
-    if len(angle_arr) < 3:
-        return False
 
     angle_arr.sort()
     for i in range(len(angle_arr) - 1):
@@ -94,11 +96,7 @@ def is_pinned(a):
 
 
 # randomly move mover around pivot.
-def step_rotate(mover: int, pivot: int):
-    # mover and pivot have to be tangent
-    if adj_matrix[mover][pivot] != 0:
-        print("Void step. Mover and pivot not tangent.")
-        return
+def step(mover: int, pivot: int, stick_rate: float):
     # find available places around pivot
     nears = calc_near(pivot)
     available = portion.openclosed(-np.pi, np.pi)
@@ -119,13 +117,10 @@ def step_rotate(mover: int, pivot: int):
 
     if available.empty:
         print("Void step. No available space next to the pivot.")
-        return
+        return circles[mover]
 
-    # TODO: this ratio should be changed into a parameter later
-    stick_rate = 1
     # stick: move mover to a place sticking to an obstacle. Notice when only 1 circle around pivot stick is not doable.
-    # TODO fix
-    if np.random.rand() < stick_rate and available != portion.openclosed(-np.pi, np.pi):
+    if np.random.rand() < stick_rate:
         stick_angles = []
         for interval in available._intervals:
             stick_angles.append(interval.lower)
@@ -133,18 +128,33 @@ def step_rotate(mover: int, pivot: int):
         if available.enclosure == portion.openclosed(-np.pi, np.pi):
             stick_angles.remove(-np.pi)
             stick_angles.remove(np.pi)
+        if len(stick_angles) == 0:
+            print("Not doable.")
+            return circles[mover]
         stick_angle = stick_angles[np.random.randint(len(stick_angles))]
         return circles[pivot][0] + np.cos(stick_angle), circles[pivot][1] + np.sin(stick_angle)
-    # TODO non-stick rotating
+
+    # non-stick: uniformly pick angle from all available angle intervals
     else:
-        pass
-    return
+        # available is 1 interval
+        if available.atomic:
+            stick_angle = available.lower + np.random.rand() * (available.upper - available.lower)
+            return circles[pivot][0] + np.cos(stick_angle), circles[pivot][1] + np.sin(stick_angle)
+        # available is union of 2 intervals
+        else:
+            stick_angle = available.lower + np.random.rand() * \
+                          (available._intervals[0].upper - available._intervals[0].lower +
+                           available._intervals[1].upper - available._intervals[1].lower)
+            if stick_angle > available._intervals[0].upper:
+                stick_angle += (available._intervals[1].lower - available._intervals[0].upper)
+            return circles[pivot][0] + np.cos(stick_angle), circles[pivot][1] + np.sin(stick_angle)
+            pass
 # step_rotate
 
 
 # calculate smallest bounding rectangle with given ratio from convex hull
-# ratio: aspect ratio length/width of rectangle
-def calc_bounding_rec(ratio: int):
+# shape: aspect ratio length/width of rectangle
+def calc_bounding_rec(shape: float):
     ch = ConvexHull(circles)
     cur_max_volume = 0
     for i in range(ch.nsimplex):
@@ -162,15 +172,15 @@ def calc_bounding_rec(ratio: int):
         cur_max_dist_axis += 1
         cur_max_dist_perp = max(cur_dist_perps) - min(cur_dist_perps) + 1
         # handle squares independently
-        if ratio == 1:
+        if shape == 1:
             cur_volume = np.power(max(cur_max_dist_axis, cur_max_dist_perp), 2)
         # not a square
-        elif cur_max_dist_axis > ratio * cur_max_dist_perp:
-            cur_volume = np.power(cur_max_dist_axis, 2) * ratio
-        elif cur_max_dist_perp > ratio * cur_max_dist_axis:
-            cur_volume = np.power(cur_max_dist_perp, 2) * ratio
+        elif cur_max_dist_axis > shape * cur_max_dist_perp:
+            cur_volume = np.power(cur_max_dist_axis, 2) * shape
+        elif cur_max_dist_perp > shape * cur_max_dist_axis:
+            cur_volume = np.power(cur_max_dist_perp, 2) * shape
         else:
-            cur_volume = np.power(min(cur_max_dist_axis, cur_max_dist_perp), 2) * ratio
+            cur_volume = np.power(min(cur_max_dist_axis, cur_max_dist_perp), 2) * shape
         if cur_volume > cur_max_volume:
             cur_max_volume = cur_volume
     return cur_max_volume
@@ -178,33 +188,44 @@ def calc_bounding_rec(ratio: int):
 
 
 # one Metropolis Monte Carlo loop
-def mc_met(k: int):
+# acc_parameter: parameter used to determine whether to accept an energy-raising step. Check Boltzmann distribution.
+def mc_met(acc: float, shape: float, stick_rate: float):
     mover = np.random.randint(num)
     if is_pinned(mover):
         mover = np.random.randint(num)
     mover_old_parameter = (circles[mover][0], circles[mover][1])
-    old_energy = calc_bounding_rec(1)
+    print("Old location: ", mover_old_parameter)
+    old_energy = calc_bounding_rec(shape)
     print("Old energy: ", old_energy)
+
     pivots = []
     for i in range(num):
         if adj_matrix[i][mover] == 0:
             pivots.append(i)
-    if not pivots:
-        return
-    j = np.random.randint(len(pivots))
-    pivot = pivots[j]
 
-    mover_new_parameter = step_rotate(mover, pivot)
+    if not pivots or len(pivots) == 1:  # TODO split this to detect lone pair move
+        ch = ConvexHull(circles)
+        for j in range(len(ch.vertices)):
+            pivots.append(j)
+    # TODO lone pair move
+    if len(pivots) == 1:
+        ch = ConvexHull(circles)
+
+    k = np.random.randint(len(pivots))
+    pivot = pivots[k]
+
+    mover_new_parameter = step(mover, pivot, stick_rate)
     circles[mover] = mover_new_parameter
-    new_energy = calc_bounding_rec(1)
+    print("New location: ", mover_new_parameter)
+    new_energy = calc_bounding_rec(shape)
     print("New energy: ", new_energy)
 
-    if new_energy <= old_energy:
+    if new_energy - old_energy <= 1e-6:
         print("Taking step with energy falling.")
         calc_adj_matrix()
         return
     else:
-        threshold = np.power(np.e, (new_energy - old_energy) * -k)
+        threshold = np.power(np.e, (new_energy - old_energy) * -acc)
         accept = np.random.rand()
         if accept < threshold:
             print("Taking step with energy raising.")
@@ -215,6 +236,7 @@ def mc_met(k: int):
         return
 # mc_met
 
+# TODO shrink all components to make them touch
 
 num = 0
 circles = np.ndarray((1,))
@@ -223,43 +245,33 @@ adj_matrix = np.ndarray((1,))
 
 if __name__ == "__main__":
     # num = int(input("Input num of circles."))
-    num = 9
+    num = 50
     adj_matrix = np.zeros((num, num))
     circles = np.zeros((num, 2))
 
-    circles[0][0] = 0
-    circles[0][1] = 0
-    circles[1][0] = 0
-    circles[1][1] = 1
-    circles[2][0] = 0
-    circles[2][1] = 2
-    circles[3][0] = 0
-    circles[3][1] = 3
-    circles[4][0] = 0
-    circles[4][1] = 4
-    circles[5][0] = 0
-    circles[5][1] = 5
-    circles[6][0] = 0
-    circles[6][1] = 6
-    circles[7][0] = 0
-    circles[7][1] = 7
-    circles[8][0] = 1
-    circles[8][1] = 0
+    for i in range(num - 1):
+        circles[i] = (0, i)
+    circles[num - 1] = (1, 0)
     '''
-    circles[0][0] = 0
-    circles[0][1] = 1
-    circles[1][0] = 1
-    circles[1][1] = 0
-    circles[2][0] = 1
-    circles[2][1] = 1
-    circles[3][0] = 1
-    circles[3][1] = 2
-    circles[4][0] = 2
-    circles[4][1] = 1
+    for i in range(2):
+        for j in range(100):
+            circles[i*100 + j] = (i, j)
     '''
-    cur_ch = ConvexHull(circles)
     calc_adj_matrix()
-    for i in range(100):
-        mc_met(1)
+    for i in range(1000000):
+        print("Step ", i)
+        mc_met(5, 3, 0.8)
 
-    pass
+        if i % 100 == 0:
+            # plotting
+            fig, ax = plt.subplots()
+            fig.set_size_inches(10, 40)
+            plt.xlim(-10, 10)
+            plt.ylim(-10, 60)
+            plt.grid(linestyle='--')
+            ax.set_aspect(1)
+
+            for i in range(num):
+                cur_circle = plt.Circle(circles[i], 0.5)
+                ax.add_artist(cur_circle)
+            plt.show()
